65eec609372ff1d3753bcb74fd29d0119efec61f
Malik Hardy <50418576+Malikhardy23@users.noreply.github.com>
2020-11-24 23:38:56 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -4,11 +4,20 @@ const feedbackMessage = document.getElementById("feedbackMessage");
 const employeeName = document.getElementById("employeeName");
 const employeeCode = document.getElementById("employeeCode");
 const departmemtOption = document.getElementById("departmentOption");
+const questionsContainer = document.getElementById("questionsContainer");
+const questions = document.getElementsByClassName('question')
+const buttons = document.getElementsByTagName('button');
+const yesBtn = document.getElementsByClassName('yesBtn');
+const noBtn = document.getElementsByClassName('noBtn');
+
+console.log(questions);
+console.log(buttons);
+console.log(questionsContainer);
+console.log(yesBtn);
+console.log(noBtn);
+
 
 // FULL NAME FUNCTION //
-
-// departmemtOption.disabled = true;
-
 
     const employeeNameFunction = (e) => {
         if(e.target){
@@ -19,7 +28,6 @@ const departmemtOption = document.getElementById("departmentOption");
     const employeeNameCodeFunction = (e) => {
         if(e.target){
             console.log(e.target.value);
-            departmemtOption.disasbled = false;
         }
     }
     
@@ -29,8 +37,29 @@ const departmemtOption = document.getElementById("departmentOption");
         }
     }
 
+
+
+    // EMPLOYEE SECTION EVENT LISTENERS //
+
     employeeName.addEventListener('input', employeeNameFunction);
     employeeCode.addEventListener('input', employeeNameCodeFunction);
     departmemtOption.addEventListener('change', departmemtOptionFunction);
+
+
+    // EMPLOYEE QUESTIONS EVENT LISTENERS //
+
+    questionsContainer.addEventListener('click', (e) => {
+        if(e.target){
+            if(e.target.textContent === 'NO'){
+                e.target.style.color = "red";
+                e.target.style.backgroundColor = "blue";
+            }
+            else if(e.target.textContent === "YES"){
+                e.target.style.color = "red";
+                e.target.style.backgroundColor = "tan";
+            }
+        }
+    }) 
+
 
 
