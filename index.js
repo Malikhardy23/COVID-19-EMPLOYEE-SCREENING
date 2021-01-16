7c60998a93959089ca3a7944a4b0ff618d4eb0e6
Malik Hardy <50418576+Malikhardy23@users.noreply.github.com>
2021-01-15 21:34:17 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,24 +1,41 @@
 // VARIABLES // 
-
 const feedbackMessage = document.getElementById("feedbackMessage");
 const employeeName = document.getElementById("employeeName");
 const employeeCode = document.getElementById("employeeCode");
 const departmemtOption = document.getElementById("departmentOption");
 const questionsContainer = document.getElementById("questionsContainer");
 const questions = document.getElementsByClassName('question')
-let yesBtn = document.getElementById("yesBtn");
-let noBtn = document.getElementById("noBtn");
 const btnContainer = document.getElementById("buttonContainer");
-const btn = document.querySelectorAll('button');
+//const btn = document.querySelectorAll('button');
+
+var questObj = {
+    question1 : false,
+    question2 : false,
+    question3 : false,
+    question4 : false 
+};
+
+let buttonContainer1 = document.getElementById("buttonContainer1");
+let buttonContainer2 = document.getElementById("buttonContainer2");
+let buttonContainer3 = document.getElementById("buttonContainer3");
+let buttonContainer4 = document.getElementById("buttonContainer4");
+
+const yesBtn1 = document.getElementById("yesBtn1");
+const noBtn1 = document.getElementById("noBtn1");
+
+const yesBtn2 = document.getElementById("yesBtn2");
+const noBtn2 = document.getElementById("noBtn2");
+
+const yesBtn3 = document.getElementById("yesBtn3");
+const noBtn3 = document.getElementById("noBtn3");
+
+const yesBtn4 = document.getElementById("yesBtn4");
+const noBtn4 = document.getElementById("noBtn4");
+
+
 
 console.log(questions);
-console.log(questionsContainer);
-console.log(yesBtn.value);
-console.log(noBtn.value);
-console.log(btn);
-
-
-
+console.log(questionsContainer);   
 
 // FULL NAME FUNCTION //
 
@@ -50,26 +67,34 @@ console.log(btn);
 
 
     // EMPLOYEE QUESTIONS EVENT LISTENERS //
+var buttonContainterArr = [buttonContainer1,buttonContainer2,buttonContainer3,buttonContainer4];
+var btnYesArray = [yesBtn1,yesBtn2,yesBtn3,yesBtn4];
+var btnNoArray = [noBtn1,noBtn2,noBtn3,noBtn4];
+var buttonContainterArr = [buttonContainer1,buttonContainer2,buttonContainer3,buttonContainer4];
+var questObjArray = [questObj.question1,questObj.question2,questObj.question3,questObj.question4];
 
 
+ for(var i = 0;i < buttonContainterArr.length;i++){
+    // anonymous function
+    (function(i){
+        buttonContainterArr[i].addEventListener('click', (e) => { 
+            // clean up and clear values before click
+           if(e.target.value === "YES" ){
+                // value isn't changing, figure out why ???
+                btnYesArray[i].style.backgroundColor = "yellow"; 
+                btnNoArray[i].disabled = true;
+                questObjArray[i] = true;
+           }else{ 
+               btnNoArray[i].style.backgroundColor = "red";  
+               btnYesArray[i].disabled = true;  
+               questObjArray[i] = false;     
+           } 
 
-    questionsContainer.addEventListener('click', (e) => {
-        for(let i = 0; i < btn.length; i++){
+       console.log(questObj);
+       }); 
 
-            if(e.target === yesBtn){
-                yesBtn.style.backgroundColor = "yellow";
-            } else {
-                noBtn.style.backgroundColor = "red";
-            } 
-        }
-    }) 
-
-    /* for (var i = 0; i < btn.length; i++) {
-        btn[i].addEventListener("click", function() {
-        var current = document.getElementsByClassName("active");
-        current[0].className = current[0].className.replace(" active", "");
-        this.className += " active";
-        });
-      } */
+    }(i));// holds the increment value so it is inscync with browser
+       
+    } 
 
 
